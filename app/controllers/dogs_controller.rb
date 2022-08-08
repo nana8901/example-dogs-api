e44dbe8bf4243c4f42f42c6ad10f66b13c25b294
Nathan Brownstein <nana8901@gmail.com>
2022-08-08 14:09:48 -0400

    added sessions controller for allowing login/auth, added dog create action

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,3 +1,20 @@
 class DogsController < ApplicationController
-  
+
+  def create
+    if authenticate_user == false
+    else
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params [:breed],
+        user: current_user
+      )
+    end
+    if dog.save
+      render dog.as_json
+    else
+      render json: {error: "Something went wrong, try again?"}
+    end
+    end
+  end
 end
